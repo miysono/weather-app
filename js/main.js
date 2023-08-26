76287c47bb8d609893141fb5544c22db23506c60
Stefan <108880675+miysono@users.noreply.github.com>
2023-08-26 17:12:32 +0300

    functionality and cleaner code

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 "use strict";
+///////////////////////////////////////////////////////
+//VARIABLES/DATA
 const key = `c4377e1d554c504fc7b74990072f97b3`;
 
+///////////////////////////
 //Elements
 const searchBtn = document.querySelector(`.search-btn`);
 const searchForm = document.querySelector(`.search-form`);
@@ -10,6 +13,7 @@ const weatherContainerWrapper = document.querySelector(
 const weatherContainer = document.querySelector(`.weather-container`);
 const loadingElem = document.querySelector(`.loader`);
 
+//////////////////////////
 //Container text/img
 const feelsLikeTemperature = document.querySelector(`.temp-feels-like`);
 const weatherDescription = document.querySelector(`.weather-img`);
@@ -18,21 +22,47 @@ const region = document.querySelector(`.zone-name`);
 const humidity = document.querySelector(`.humidity-text`);
 const wind = document.querySelector(`.wind-text`);
 
-//EVENT LISTENER METHOD
+/////////////////////////////////////////////////////////
+
+//EVENT LISTENER METHODS
 const searchLogic = () => {
-  console.log(`clicked on btn`);
   const inputValue = searchForm.value;
   renderWeatherData(inputValue);
 };
-//Event listener for the
-searchBtn.addEventListener(`click`, searchLogic);
 
+//LISTENERS
+
+//Event listener for the input button
+searchBtn.addEventListener(`click`, searchLogic);
+//If user presses on enter instead
+window.addEventListener(`keydown`, function (e) {
+  if (e.key === `Enter` && searchForm.value) searchLogic();
+  else return;
+});
+
+//////////////////////////////////////////////////
+//PROMISES
+const getUserCoordinates = new Promise(function (resolve, reject) {
+  if (!navigator.geolocation) reject(`Cannot locate you. Try agian`);
+  navigator.geolocation.getCurrentPosition(function (position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    resolve([latitude, longitude]);
+  });
+});
+
+processUserData();
+
+//////////////////////////////////////////////////////
+//METHODS
+
+///////////////////
 //ASYNC METHODS
 
 async function getAreaCoords(area) {
   try {
     const response = await fetch(
-      `http://api.openweathermap.org/geo/1.0/direct?q=${area}&limit=5&appid=c4377e1d554c504fc7b74990072f97b3`
+      `https://api.openweathermap.org/geo/1.0/direct?q=${area}&limit=1&appid=c4377e1d554c504fc7b74990072f97b3`
     );
     //CHECK FOR ERROR AFTER API CALL
     if (!response.ok) throw new Error(`Unexpected error! Try again!`);
@@ -40,7 +70,6 @@ async function getAreaCoords(area) {
     const data = await response.json();
     //CHECK FOR ANOTHER ERROR IF DATA IS NOT AVAILABLE
     if (!data.length) throw new Error(`Invalid name! Try again!`);
-
     //return values from data arr;
     const { lat, lon } = data[0];
     return [lat, lon];
@@ -69,17 +98,43 @@ async function getZoneWeatherData(zone) {
 
 async function renderWeatherData(inputValue) {
   try {
-    //add the spinner while function is processing
+    //adds the spinner while function is processing
     loading();
+    changeSearchFormMsg(`Search...`);
     const weatherData = await getZoneWeatherData(inputValue);
     assignAPIValues(weatherData);
     loading();
   } catch (err) {
-    console.log(err);
+    loading();
+    processErr(err);
   }
 }
 
-// METHODS
+async function renderUserWeatherData(coords) {
+  try {
+    loading();
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${coords[0]}&lon=${coords[1]}&appid=c4377e1d554c504fc7b74990072f97b3&units=metric`
+    );
+    const data = await response.json();
+    assignAPIValues(data);
+    loading();
+  } catch (err) {
+    processErr(err);
+  }
+}
+
+async function processUserData() {
+  try {
+    const userCoords = await getUserCoordinates;
+    renderUserWeatherData(userCoords);
+  } catch (err) {
+    throw err;
+  }
+}
+
+/////////////
+// SIMPLE METHODS
 
 function assignAPIValues(weatherData) {
   //remove the input value;
@@ -91,6 +146,8 @@ function assignAPIValues(weatherData) {
     weatherData.main.feels_like
   )}°c`;
   console.log(weatherData);
+  weatherDescription.src = `/weather-imgs/${weatherData.weather[0].main}.png`;
+
   temperature.textContent = `${convertToCelsius(weatherData.main.temp)}°c`;
   region.textContent = weatherData.name;
   humidity.textContent = `${weatherData.main.humidity}%`;
@@ -101,8 +158,16 @@ function convertToCelsius(temperature) {
   return Math.round(temperature);
 }
 
+function processErr(err) {
+  searchForm.value = ``;
+  changeSearchFormMsg(err);
+}
+
+function changeSearchFormMsg(msg) {
+  searchForm.setAttribute(`placeholder`, msg);
+}
+
 function loading() {
-  weatherContainer.classList.toggle(`result-loading`);
   weatherContainerWrapper.classList.toggle(`hidden`);
   loadingElem.classList.toggle(`hidden`);
 }
